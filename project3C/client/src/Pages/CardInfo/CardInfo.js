1d2ecc5600ff6be4d8070dcd2bd0eb5a7bc63b61
Jacob Bailey <jayembailey@gmail.com>
2023-06-22 19:26:46 -0400

    number spacing achieved

--- project3C/client/src/Pages/CardInfo/CardInfo.js
+++ project3C/client/src/Pages/CardInfo/CardInfo.js
@@ -3,15 +3,16 @@ import './CardInfo.css';
 import checkMark from '../../images/icon-complete.svg';
 import ExpirationCheck from '../../Functions/ExpirationCheck';
 import ValidateCardNumber from '../../Functions/validateCardNumber';
-// import getData from '../../Functions/Routes';
 import Axios from 'axios';
 
 const CardInfo = () => {
     const [name, setName] = useState(null);
     const [number, setNumber] = useState(null);
+    const [formattedNumber, setFormattedNumber] = useState('0000 0000 0000 0000');
     const [expMonth, setExpMonth] = useState(null);
     const [expYear, setExpYear] = useState(null);
     const [cvc, setCVC] = useState(null);
+    const [provider, setProvider] = useState(null)
     const [errorMessage, setErrorMessage] = useState(null);
     const [paymentSuccessful, setPaymentSuccessful] = useState(false);
     const [allCardsInfo, setAllCardsInfo] = useState([]);
@@ -115,9 +116,58 @@ const CardInfo = () => {
         setIsLoading(false);  
     }
 
+    function ccFormat(e) {
+        let numString = e;
+        let typeCheck = numString.substring(0,2);
+        let cType = '';
+        let block1 = '';
+        let block2 = '';
+        let block3 = '';
+        let block4 = '';
+        let formatted = '';
+
+        if (typeCheck.length === 2) {
+            typeCheck = parseInt(typeCheck);
+            if (typeCheck >=40 && typeCheck <= 49) {
+                cType = 'Visa';
+            } else if (typeCheck >=51 && typeCheck <= 55) {
+                cType = 'MC';
+            } else if (typeCheck >=60 && typeCheck <= 62) {
+                cType = 'Discover';
+            } else if (typeCheck === 34 || typeCheck === 37) {
+                cType = 'AMEX';
+            }
+        }
+        setProvider(cType)
+        block1 = numString.substring(0,4)
+        if (block1.length === 4) {
+            block1 = block1 + ' ';
+        }
+        if (cType === 'Visa' || cType === 'MC' || cType === 'Discover') {
+            block2 = numString.substring(4,8);
+            if (block2.length === 4) {
+                block2 = block2 + ' ';
+            }
+            block3 = numString.substring(8,12);
+            if (block3.length === 4) {
+                block3 = block3 + ' ';
+            }
+            block4 = numString.substring(12,16);
+        }
+        if (cType === 'AMEX') {
+            block2 = numString.substring(4,10);
+            if (block2.length === 6) {
+                block2 = block2 + ' ';
+            }
+            block3 = numString.substring(10,15)
+        }
+
+        formatted = block1 + block2 + block3 + block4;
+        setFormattedNumber(formatted)
+    }
+
     return (
         <>
-        {/* {getData()} */}
             <div className="big-container">
                 <nav className="navbar">
                     STACKMONSTERS!
@@ -132,7 +182,7 @@ const CardInfo = () => {
                             <p className="name-output" id="name-output">{name}</p>
                         </div>
                         <div className="number-display">
-                            <p className="number-output" id="number-output">{number}</p>
+                            <p className="number-output" id="number-output">{formattedNumber}</p>
                         </div>
                         <div className="exp-m-display">
                             <p className="exp-m-output" id="exp-m-output">{expMonth}/{expYear}</p>
@@ -187,23 +237,23 @@ const CardInfo = () => {
                             <>
                                 <label className='cardHolderNameLabel'>
                                     CARDHOLDER NAME:
-                                    <input name='cardHolderName' className='cardHolderName' type='text' placeholder='e.g. Jane Appleseed' pattern='^[a-zA-Z]{2,40} [a-zA-Z]{2,40}$' maxLength='24' autoComplete='name' required defaultValue={name} onChange={(e) => setName(e.target.value)}/>
+                                    <input name='cardHolderName' className='cardHolderName' type='text' placeholder='e.g. Jane Appleseed' pattern='^[a-zA-Z]{2,40} [a-zA-Z]{2,40}$' maxLength='24' autoComplete='name'  defaultValue={name} onChange={(e) => setName(e.target.value)} />
                                 </label>
                                 <label className='cardHolderNumberLabel'>
                                     CARDHOLDER NUMBER:
-                                    <input name='cardHolderNumber' className='cardHolderNumber' type='text' pattern='[0-9]{16}' maxLength="16" placeholder='e.g. 1234 5678 9123 0000' autoComplete='off' required defaultValue={number} onChange={(e) => setNumber(e.target.value)}/>
+                                    <input name='cardHolderNumber' className='cardHolderNumber' type='text' pattern='[0-9]{16}' maxLength="16" placeholder='e.g. 1234 5678 9123 0000' autoComplete='off'  defaultValue={number} onChange={(e) => {setNumber(e.target.value);ccFormat(e.target.value)}}/>
                                 </label>
                                 <div className="expAndCvcBox">
                                     <label className='cardHolderExpDateLabel'>
                                         EXP. DATE (MM/YY):
                                         <div style={{display: 'flex'}}>
-                                            <input name='cardHolderExpMonth' className='cardHolderExpMonth' type='text' placeholder='MM' pattern="[0-9]{2}" maxLength="2" autoComplete='off' required defaultValue={expMonth} onChange={(e) => setExpMonth(e.target.value)}/>
-                                            <input name='cardHolderExpYear' className='cardHolderExpYear' type='text' placeholder='YY' pattern="[0-9]{2}" maxLength="2" autoComplete='off' required defaultValue={expYear} onChange={(e) => setExpYear(e.target.value)}/>
+                                            <input name='cardHolderExpMonth' className='cardHolderExpMonth' type='text' placeholder='MM' pattern="[0-9]{2}" maxLength="2" autoComplete='off'  defaultValue={expMonth} onChange={(e) => setExpMonth(e.target.value)}/>
+                                            <input name='cardHolderExpYear' className='cardHolderExpYear' type='text' placeholder='YY' pattern="[0-9]{2}" maxLength="2" autoComplete='off'  defaultValue={expYear} onChange={(e) => setExpYear(e.target.value)}/>
                                         </div>
                                     </label>
                                     <label className='cardHolderCVCLabel'>
                                         CVC:
-                                        <input name='cardHolderCVCMonth' className='cardHolderCVCMonth' type='text' placeholder='e.g 123' pattern="[0-9]{3}" maxLength="3" autoComplete='off' required defaultValue={cvc} onChange={(e) => setCVC(e.target.value)}/>
+                                        <input name='cardHolderCVCMonth' className='cardHolderCVCMonth' type='text' placeholder='e.g 123' pattern="[0-9]{3}" maxLength="3" autoComplete='off'  defaultValue={cvc} onChange={(e) => setCVC(e.target.value)}/>
                                     </label>
                                 </div>
                                 <button type="submit" onClick={e => validationCheck(e)}>CONFIRM</button>
@@ -215,7 +265,7 @@ const CardInfo = () => {
                     </form>
                 </div>
                 <div className='footer'>
-                    <p>Start Date/Time: 06/13/23 1200ET</p>
+                    <p>Start Date/Time: 06/21/23 2000ET</p>
                 </div>
             </div>
         </>
