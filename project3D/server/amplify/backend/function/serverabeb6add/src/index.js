045003f485695673153133e534b2fac3511712a2
Jon204D <rd.jonathan.d@hotmail.com>
2023-08-08 00:58:57 -0400

    Pushing Test

--- project3D/server/amplify/backend/function/serverabeb6add/src/index.js
+++ project3D/server/amplify/backend/function/serverabeb6add/src/index.js
@@ -29,14 +29,15 @@ Amplify Params - DO NOT EDIT */
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+  const body = {
+      message: "Hello from Lambda"
+  }
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify(body),
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+      }
+  };
+  return response;
 };
