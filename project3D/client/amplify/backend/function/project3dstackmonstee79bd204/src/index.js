26edf2f2de291fb45023f6b6cfc28cb3bc7d7fd3
Jacob Bailey <jayembailey@gmail.com>
2023-08-24 12:41:36 -0400

    attempt to reconnect amplify backend

---
+++ project3D/client/amplify/backend/function/project3dstackmonstee79bd204/src/index.js
@@ -0,0 +1,38 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["secret"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	FUNCTION_PROJECT3DSTACKMONSTE119A4516_NAME
+	API_API99EBF717_APINAME
+	API_API99EBF717_APIID
+	DB_User
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
--- project3D/client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ project3D/client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "api88d68e1e": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "api99ebf717": {
       "ApiId": "string",
       "ApiName": "string",
@@ -9,6 +14,14 @@ export type AmplifyDependentResourcesAttributes = {
   "function": {
     "project3dstackmonste119a4516": {
       "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "project3dstackmonstee79bd204": {
+      "Arn": "string",
+      "CloudWatchEventRule": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
